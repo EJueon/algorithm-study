23716653f1769a64d4085163e5fe6c560e9615ef
EJueon <93572176+EJueon@users.noreply.github.com>
2022-10-02 17:01:22 +0900

    [Silver I] Title: 신입 사원, Time: 7184 ms, Memory: 217832 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,19 @@
+test_num = int(input())
+
+for n in range(test_num):
+    p_count = int(input())
+    p_list = []
+    for c in range(p_count):
+        M, K = map(int, input().split())
+        p_list.append([M, K])
+    p_list.sort()
+    fail = 0
+    p_max = p_count
+    for p in p_list:
+        if p[1] < p_max:
+            p_max = p[1]
+        elif p[1] > p_max:
+            fail += 1
+    print(f'{p_count - fail}')
+
+            
