8296786db57647e3a901f7b29cbf31115dcaf7f4
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-02-10 15:20:55 +0900

    [Gold V] Title: AC, Time: 224 ms, Memory: 41344 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def process(P, arr):
+    r_flag = False
+    queue = deque(arr)
+    try:
+        for p in P:
+            if p == 'R':
+                r_flag = not r_flag
+            elif p == 'D':
+                if not r_flag:
+                    queue.popleft()
+                else:
+                    queue.pop()
+    except:
+        return "error"
+    
+    arr = list(queue)
+    if r_flag:
+        arr = reversed(arr)
+        
+    return f"[{','.join(list(map(str, arr)))}]"
+
+T = int(input())
+for _ in range(T):
+    P = list(input())
+    n = int(input())
+    arr = input().split('\n')[0][1:-1].split(',')
+    if '' in arr and len(arr) == 1:
+        arr = []
+    else:
+        arr = list(map(int, arr))
+    print(process(P, arr))
