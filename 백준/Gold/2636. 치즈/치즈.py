c2248db652aec39f352e5c258d500576e0fc6c1e
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-02-10 14:32:09 +0900

    [Gold IV] Title: 치즈, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# 공기를 중심으로 치즈 겉을 찾아서 제거 
+R, C = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(R)]
+DELTAS = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+def bfs(R, C, y, x):
+    queue = deque([(y, x)])
+    visited = [[0] * C for _ in range(R)]
+    while queue:
+        y, x = queue.popleft()
+        for dy, dx in DELTAS:
+            ny = y + dy
+            nx = x + dx
+            if ny < 0 or ny >= R or nx < 0 or nx >= C:
+                continue
+            if visited[ny][nx] == 1:
+                continue
+            if board[ny][nx] == 1: 
+                board[ny][nx] = 0
+            else:
+                queue.append((ny, nx))
+            visited[ny][nx] = 1
+    
+    return sum([sum(row) for row in board])
+
+
+cheese_cnt = sum([sum(row) for row in board])
+if cheese_cnt == 0:
+    print(0)
+    print(0)
+else:
+    cheese_cnts = [cheese_cnt]
+    while True:
+        cnt = bfs(R, C, 0, 0)
+        if cnt == 0:
+            print(len(cheese_cnts))
+            print(cheese_cnts[-1])
+            break
+        else:
+            cheese_cnts.append(cnt)
