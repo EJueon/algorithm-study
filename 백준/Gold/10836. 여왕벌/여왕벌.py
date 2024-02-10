a3ea50c7eef60ce772172ffb8b7c556b0affbf69
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-02-10 18:57:39 +0900

    [Gold IV] Title: 여왕벌, Time: 2708 ms, Memory: 115328 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -2,41 +2,39 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-orders = [list(map(int, input().split())) for _ in range(M)]
-board = [[1] * N for _ in range(N)]
+order_arr = [0] * (2 * N - 1)
+for _ in range(M):
+    o1, o2, o3 = map(int, input().split())
+    for i in range(o1, o1 + o2):
+        order_arr[i] += 1
+    for i in range(o1 + o2, 2 * N - 1):
+        order_arr[i] += 2
 
-def change_std(order, std):
-    while std < 2 and order[std] == 0:
-        std += 1
-    return std
+board = [[1] * N for _ in range(N)]
 
 def print_board(board):
     for row in board:
         print(' '.join(list(map(str, row))))
     
-
-def grow_by_orders(N, board, order):
-    std = 0
+def grow_by_orders(N, board, order_arr):
+    cnt = 0
     # 열 배당
-    
     for i in range(N-1, -1, -1):
-        std = change_std(order, std)
-        board[i][0] += std
-        order[std] -= 1
+        board[i][0] += order_arr[cnt]
+        cnt += 1
         
     for i in range(1, N):
-        std = change_std(order, std)
-        board[0][i] += std
-        order[std] -= 1
+        board[0][i] += order_arr[cnt]
+        cnt += 1
 
 def grow_relative(N, board):
     for y in range(1, N):
         for x in range(1, N):
             # check L D U
-            board[y][x] = max(board[y][x-1], board[y-1][x-1], board[y-1][x])
-            
-for order in orders:
-    grow_by_orders(N, board, order)
+            board[y][x] = board[0][x]
+    
+final_order = [0, 0, 0]      
+grow_by_orders(N, board, order_arr)
 grow_relative(N, board)
     
 
