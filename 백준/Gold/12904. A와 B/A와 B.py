36d0b3d4b308f0794e4b91d0bd568495eeb1d699
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-02-18 20:22:50 +0900

    Update A와 B.py

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,18 +1,8 @@
-import sys
 from itertools import permutations
 input = sys.stdin.readline
 
 S = list(input().split()[0])
 T = list(input().split()[0])
-
-def add(s, x):
-    return s + [x]
-
-def op_1(s):
-    return add(s, 'A')
-    
-def op_2(s):
-    return add(s[::-1], 'B')
 
 def solution(S, T):
     
@@ -22,6 +12,7 @@ def solution(S, T):
         elif T[-1] == 'B':
             T.pop()
             T = T[::-1]
+            
     if S == T:
         return 1
     else:
