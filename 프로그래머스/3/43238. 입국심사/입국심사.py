d5f7293b22940898e7e143d822d78442e7fa2f9e
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-02-19 08:40:37 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,23 @@
 def solution(n, times):
     answer = 0
-    left, right = 1, max(times) * n
     
-    while left <= right:
-        mid = (left + right) // 2
-        people = 0
+    start, end = 0, max(times) * n
+    while start <= end:
+        
+        mid = (start + end) // 2
+        processed_cnt = 0
         
         for time in times:
-            people += mid // time
-            if people >= n: break
-        if people >= n:
-            answer = mid 
-            right = mid - 1
+            processed_cnt += mid // time
+            if processed_cnt >= n:
+                break
+        
+        if processed_cnt >= n: 
+            # 해당 시간이 충분히 모든 사람을 커버할 경우 최소 시간을 구해야함
+            answer = mid
+            end = mid - 1
         else:
-            left = mid + 1
+            # 해당 시간이 모든 사람을 처리하는 데 충분하지 않음
+            start = mid + 1
+            
     return answer
