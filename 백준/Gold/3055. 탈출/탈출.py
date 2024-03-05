8fbe0327b884b636b0e1ccef32b442627d51e3fa
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-03-05 23:23:52 +0900

    Update 탈출.py

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -6,12 +6,12 @@ DELTAS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 R, C = map(int, input().split())
 board = [list(input().split()[0]) for _ in range(R)]
 visited = [[-1] * C for _ in range(R)]
-w_visited = [[-1] * C for _ in range(R)]
+w_visited = [[-1] * C for _ in range(R)] # water visited
 START, END, WATERS = None, None, deque()
 
 # 물은 매분마다 인접한 비어있는 칸으로 확장함 
-
 def add_waters(board, waters, visited):
+    # bfs
     queue = deque(waters)
     while queue:
         y, x = queue.popleft()
@@ -26,7 +26,7 @@ def add_waters(board, waters, visited):
 
 
 def move(board, visited, start, end, w_visited):
-    # dfs
+    # bfs
     queue = deque([start])
     visited[start[0]][start[1]] = 0
     while queue:
@@ -37,6 +37,7 @@ def move(board, visited, start, end, w_visited):
                continue
             if board[ny][nx] in ['X', '*'] or visited[ny][nx] > -1: 
                continue
+            # w_visited 로 언제 물이 확장되었는지 확인. 이때 -1로 초기화 하였기 때문에 예외처리
             if visited[y][x] + 1 >= w_visited[ny][nx] and w_visited[ny][nx] != -1:
                 continue
             visited[ny][nx] = visited[y][x] + 1
