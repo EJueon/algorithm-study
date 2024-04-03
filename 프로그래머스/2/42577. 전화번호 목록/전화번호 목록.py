e5958a42c18402bab11d2bb121992327b2d9974c
Jueon Eom <93572176+EJueon@users.noreply.github.com>
2024-04-04 08:55:53 +0900

    [level 2] Title: 전화번호 목록, Time: 80.48 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,9 @@
 def solution(phone_book):
     answer = True
-    phone_book.sort()
+    # 앞 배열의 번호길이가 가장 짧아져야 하므로 정렬 필수 
+    phone_book.sort() 
     for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
+        if p2[:len(p1)] == p1: # 또는 p2.startswith(p1)
             answer = False
             break
     
